67db4180c58150462e2263e15647ac9c6203bedb
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-12-20 13:06:36 +0530

    Add:To restrict duplicate entries in the address book

--- src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookMain.java
@@ -1,8 +1,11 @@
 package com.bridgelabz.addressbook;
+import java.util.Scanner;
 
 public class AddressBookMain {
-    public static void main(String[] args) {
-		System.out.println(" Welcome To Address Book Program!");
+  public static Scanner scannerObject = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println(" Welcome To Address Book Program!! ");
+		
 		AddressBookDirectory addressBookDirectory = new AddressBookDirectory();
         addressBookDirectory.operationDirectory();
         AddressBook addressBook = new AddressBook();
