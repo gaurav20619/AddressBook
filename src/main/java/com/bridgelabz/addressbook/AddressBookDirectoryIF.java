cc5814cb99af4468c9dc129ec8f6afed4b5483a2
gaurav20619 <gauravsinghraijada619@gmail.com>
2022-12-20 13:38:03 +0530

    Add: To view person by city or state in multiple address book.

--- src/main/java/com/bridgelabz/addressbook/AddressBookDirectoryIF.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBookDirectoryIF.java
@@ -4,5 +4,7 @@ public interface AddressBookDirectoryIF {
     public  void addAddressBook();
     public void operationDirectory();
     public void displayDirectoryContents();
-    public void editAddressBook();  
+    public void editAddressBook();
+    public void searchByCity();
+    public void searchByState();
 }
